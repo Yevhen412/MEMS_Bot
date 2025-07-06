a1198eae83699203827f826c8ef0b180545980a9
Yevhen412 <neogen123456789@gmail.com>
2025-07-06 17:12:37 +0200

    Create pump_scanner.py

---
+++ pump_scanner.py
@@ -0,0 +1,8 @@
+def get_recent_pump_tokens():
+    # Здесь должна быть реальная интеграция с API Pump.fun или Bitquery
+    return [{
+        "symbol": "MEME123",
+        "mint": "So1aNaMintAddressExample",
+        "url": "https://pump.fun/So1aNaMintAddressExample",
+        "liquidity_usd": 1500
+    }]
